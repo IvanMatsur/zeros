a51c6faf9df7bb9f7fd660c1a1e5bc0fd3dc3d89
Ivan Matsur <ivan.matsur@yandex.ru>
2018-09-22 23:08:43 +0300

    Realize 'getZerosCount' function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+
+  while(number > 5) {
+    result += parseInt(number / 5);
+    number = number / 5;
+  } 
+  
+  return result;
 }
